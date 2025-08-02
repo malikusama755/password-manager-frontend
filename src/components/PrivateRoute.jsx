402083f6ebdc370fe277c5a1015e30f8666d9148
malikusama755 <malikusama755@gmail.com>
2025-08-02 20:45:12 +0500

    Initial commit

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,9 +3,9 @@ import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { token } = useContext(AuthContext);
 
-  if (!user) {
+  if (!token) {
     return <Navigate to="/login" replace />;
   }
 
